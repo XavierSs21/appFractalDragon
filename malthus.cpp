ed51bd736ca8057ce8f51f6a3d2a863f693d3502
Xavier <xavier.sotomayor21@hotmail.com>
2025-03-23 23:19:11 -0600

    Arregla metodo dibujar e implementa generarMalthus

--- malthus.cpp
+++ malthus.cpp
@@ -21,20 +21,27 @@ void malthus::dibujar(QPainter *canvas) {
 
     int contador = 0;
 
-    canvas->setPen(QColor(186, 43, 26));
+
 
     for(int i = 1; i <= 15; i++){
 
         for(int j = 1; j <= 10; j++){
             Razon += 0.01;
             PobAnt = 0.01;
-            for (int k = 1; k <= 1000; i++){
+
+            int color = QColor::fromHsv((j * 17) % 360, 255, 255).rgb();
+            canvas->setPen(QColor(color));
+
+            for (int k = 1; k <= 1000; k++){
                 PobNueva = Razon * ( PobAnt * ( 1 - PobAnt ) );
                 x = PobNueva - PobAnt;
-                const auto & newX = x * width / 2;
-                const auto & newY = ( height / 2 ) - ( y * height / 2 );
+
+                const auto & newX = static_cast<int>((x * width / 2) + width / 2);
+                const auto & newY = static_cast<int>((height / 2) - (y * height / 2));
+
                 canvas->drawPoint( newX, newY );
-                if( PobAnt == PobNueva )
+
+                if( std::abs(PobAnt - PobNueva) < 1e-6 ) // Evitar falsos positivos
                     contador++;
                 else
                     contador = 0;
@@ -48,4 +55,8 @@ void malthus::dibujar(QPainter *canvas) {
 
     }
 
+}
+
+void malthus::generarMalthus(QPainter *canvas) {
+    dibujar(canvas);
 }
