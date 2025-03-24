9a4a24413579dde76b129e2fc73264a0203ee69d
Xavier <xavier.sotomayor21@hotmail.com>
2025-03-24 04:34:40 -0600

    Agrega logica del metodo dibujar para fractal de Mandel

--- mandel.cpp
+++ mandel.cpp
@@ -2,4 +2,52 @@
 
 Mandel::Mandel(QObject *parent)
     : QObject{parent}
-{}
+{
+    Limite = 10;
+    origX = -2.0;
+    origY = -1.25;
+    dimX = 0.5;
+    dimY = 1.25;
+}
+
+Mandel::~Mandel() { }
+
+void Mandel::dibujar(QPainter *canvas) {
+
+    const int width = canvas->window().width();
+    const int height = canvas->window().height();
+
+    stepX = (dimX - origX) / width;
+    stepY = (dimY - origY) / height;
+
+    for (int i = 0; i <= width; i++) {
+        for (int j = 0; j <= height; j++) {
+            posX = origX + i * stepX;
+            posY = origY + j * stepY;
+
+            iterX = 0.0;
+            iterY = 0.0;
+            Steps = 0;
+            Terminar = 0;
+
+            while (!Terminar) {
+                tempX = (iterX * iterX - iterY * iterY) + posX;
+                iterY = 2 * iterX * iterY + posY;
+                iterX = tempX;
+
+                Steps++;
+
+                if (std::hypot(iterX, iterY) >= 2.0 || Steps >= Limite)
+                    Terminar = 1;
+            }
+
+            if (Steps < Limite) {
+                int intensidad = static_cast<int>(255.0 * Steps / Limite);
+                canvas->setPen(QColor::fromRgb(intensidad, intensidad, intensidad));
+                canvas->drawPoint(i, j);
+            }
+        }
+    }
+
+
+}
--- mandel.h
+++ mandel.h
@@ -2,12 +2,20 @@
 #define MANDEL_H
 
 #include <QObject>
+#include <QPainter>
 
 class Mandel : public QObject
 {
     Q_OBJECT
 public:
     explicit Mandel(QObject *parent = nullptr);
+    ~Mandel();
+
+    int Limite, Steps, Terminar;
+    double stepX, stepY, posX, posY, origX, origY;
+    double dimX, dimY, iterX, iterY, tempX;
+
+    void dibujar(QPainter *canvas);
 
 signals:
 };
